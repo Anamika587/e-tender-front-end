80391537da1abfacb5f663241df9d49152e13b39
Anamika587 <kumarianamika587@gmail.com>
2021-09-04 14:57:19 +0530

    tree struct

--- src/components/TreeStruct.vue
+++ src/components/TreeStruct.vue
@@ -1,30 +1,98 @@
 <template>
-  <div class="hello">
-    <h1>This is tree structure</h1>
-    
+  <div class="TreeStruct">
+    <h1>Tree Structure</h1>
+    <v-jstree :data="data"  multiple allow-batch whole-row @item-click="itemClick" @item-toggle="toggleHandle"></v-jstree>
   </div>
 </template>
-
 <script>
-export default {
-  name: 'TreeStruct'
-}
-</script>
+import VJstree from 'vue-jstree'
+  export default {
+    components:{
+      VJstree
+    },
+     data(){
+       return{
+    data: [
+      {
+        "text": "Same but with checkboxes",
+        "children": [
+          {
+            "text": "initially selected",
+            "selected": true
+          },
+          {
+            "text": "custom icon",
+            "icon": "fa fa-warning icon-state-danger"
+          },
+          {
+            "text": "initially open",
+            "icon": "fa fa-folder icon-state-default",
+            "opened": true,
+            "children": [
+              {
+                "text": "Another node"
+              }
+            ]
+          },
+          {
+            "text": "custom icon",
+            "icon": "fa fa-warning icon-state-warning"
+          },
+          {
+            "text": "disabled node",
+            "icon": "fa fa-check icon-state-success",
+            "disabled": true
+          }
+        ]
+      },
+      {
+        "text": "Same but with checkboxes",
+        "opened": true,
+        "children": [
+          {
+            "text": "initially selected",
+            "selected": true
+          },
+          {
+            "text": "custom icon",
+            "icon": "fa fa-warning icon-state-danger"
+          },
+          {
+            "text": "initially open",
+            "icon": "fa fa-folder icon-state-default",
+            "opened": true,
+            "children": [
+              {
+                "text": "Another node"
+              }
+            ]
+          },
+          {
+            "text": "custom icon",
+            "icon": "fa fa-warning icon-state-warning"
+          },
+          {
+            "text": "disabled node",
+            "icon": "fa fa-check icon-state-success",
+            "disabled": true
+          }
+        ]
+      },
+      {
+        "text": "And wholerow selection"
+      }
+    ]
+  }
+     },
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+  methods: {
+    itemClick (node) {
+      console.log(node.model.text + ' clicked !')
+    }
+  },
+  toggleHandle(node,item){
+    console.log("node:",node)
+    console.log("item:",item)
+  }
+  }
+</script>
